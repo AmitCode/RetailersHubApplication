65b0734d3549e3d0153693c4e7d535f020a14758
AmitCode <amitkumar.pandey052001@gmail.com>
2026-09-23 13:06:43 +0530

    Implementing Service Discovery Eureka Server for RetailersHubApplication

---
+++ ServiceDiscoveryEurekaServer/src/main/java/com/service/discovery/ServiceDiscoveryEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.service.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDiscoveryEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDiscoveryEurekaServerApplication.class, args);
+	}
+
+}
