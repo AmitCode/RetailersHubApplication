30910a92fb94ff61b4162d1391e648d3ac255aa5
AmitCode <amitkumar.pandey052001@gmail.com>
2026-09-25 12:35:33 +0530

    Implemented custom gateway routing

--- ApiGatewayServer/src/main/java/com/api/gateway/ApiGatewayServerApplication.java
+++ ApiGatewayServer/src/main/java/com/api/gateway/ApiGatewayServerApplication.java
@@ -2,6 +2,9 @@ package com.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayServerApplication {
@@ -10,4 +13,21 @@ public class ApiGatewayServerApplication {
 		SpringApplication.run(ApiGatewayServerApplication.class, args);
 	}
 
+    @Bean
+    public RouteLocator retailersHubAppRoutingConfig(RouteLocatorBuilder routeLocatorBuilder){
+        return routeLocatorBuilder.routes()
+                .route(p -> p
+                        .path("/auth-application/authService/**")
+                        .filters(f -> f.rewritePath("/auth-application/?(?<remaining>.*)", "/${remaining}"))
+                        .uri("lb://AUTH-APPLICATION"))
+                .route(p -> p
+                        .path("/userservice-application/userService/**")
+                        .filters(f -> f.rewritePath("/userservice-application/?(?<remaining>.*)", "/${remaining}"))
+                        .uri("lb://NOTIFICATION-APPLICATION"))
+                .route(p -> p
+                        .path("/notification-application/**")
+                        .filters(f -> f.rewritePath("/notification-application/?(?<remaining>.*)", "/${remaining}"))
+                        .uri("lb:auth-application"))
+                .build();
+    }
 }
